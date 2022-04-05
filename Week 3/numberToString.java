65565d62a8e1021753f896733a87bbaa46669502
Muskan Gupta <65766138+muskangupta4@users.noreply.github.com>
2022-04-05 20:12:42 +0530

    Create numberToString.java

---
+++ Week 3/numberToString.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+
+    public static String numberToString(int num) {
+        String s=Integer.toString(num);
+        return s;
+    }
+
+    public static void main(String args[]) {
+        assert (numberToString(11).equals("11"));
+    }
+}
